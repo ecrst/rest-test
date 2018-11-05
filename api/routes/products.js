4f8e0f61816ee5d399995e1d30025f8927fdc8cd
Eugene Crystal <ugncry@gmail.com>
2018-11-05 14:47:40 +0300

    add: body-parser, cors

--- api/routes/products.js
+++ api/routes/products.js
@@ -8,8 +8,13 @@ router.get('/', (req, res, next) => {
 })
 
 router.post('/', (req, res, next) => {
+  const product = {
+    name: req.body.name,
+    price: req.body.price
+  }
   res.status(201).json({
-    message: 'post from /products'
+    message: 'post from /products',
+    product
   })
 })
 
--- app.js
+++ app.js
@@ -1,15 +1,31 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
 
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
 
 app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Cors
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', '*');
+  if (req.method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+    return res.status(200).json({});
+  }
+  next();
+}); 
 
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
 
+
+// Error handling
 app.use((req, res, next) => {
   const error = new Error('Such route is not found');
   error.status = 404;
