1e3238c2eb24d87ba2d41de91d09db713d4591e8
Eugene Crystal <ugncry@gmail.com>
2018-11-06 11:40:19 +0300

    feat: db CRUD

--- api/routes/products.js
+++ api/routes/products.js
@@ -1,46 +1,94 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const router = express.Router();
 
+const Product = require('../models/product');
+
 router.get('/', (req, res, next) => {
-  res.status(200).json({
-    message: 'get from /products'
-  })
+  Product.find()
+    .exec()
+    .then(result => {
+      if (result.length > 0) {
+        res.status(200).json(result);
+      } else {
+        res.status(404).json({ error: 'Not found' });
+      }
+    })
+    .catch(error => {
+      res.status(500).json({
+        error
+      });
+    });
 })
 
 router.post('/', (req, res, next) => {
-  const product = {
+  const product = new Product({
+    _id: new mongoose.Types.ObjectId(),
     name: req.body.name,
     price: req.body.price
-  }
-  res.status(201).json({
-    message: 'post from /products',
-    product
   })
-})
+  product
+    .save()
+    .then(result => {
+      res.status(201).json({
+        message: 'success',
+        resultObject: result
+      });
+    })
+    .catch(err => {
+      res.status(500).json({
+        error: err
+      });
+    });
+});
 
 router.get('/:productId', (req, res, next) => {
   const id = req.params.productId;
-  res.status(200).json({
-    message: 'Get /w id',
-    id 
-  })
-})
+  Product.findById(id)
+    .exec()
+    .then(result => {
+      if (result) {
+        res.status(200).json(result);
+      } else {
+        res.status(404).json({ error: 'Not found' });
+      }
+    })
+    .catch(error => {
+      res.status(500).json({
+        error
+      });
+    });
+});
 
 router.patch('/:productId', (req, res, next) => {
   const id = req.params.productId;
-  res.status(200).json({
-    message: 'Patch /w id',
-    id 
-  })
-})
+  const updateOps = {};
+  for (const ops of req.body) {
+    updateOps[ops.propName] = ops.value;
+  }
+  Product.update({ _id: id }, { $set: updateOps })
+    .exec()
+    .then(result => {
+      Product.findById(id).exec()
+        .then(result => {
+          res.status(200).json(result);
+        });
+    })
+    .catch(error => {
+      res.status(500).json({error});
+    });
+});
 
 router.delete('/:productId', (req, res, next) => {
   const id = req.params.productId;
-  res.status(200).json({
-    message: 'Delete /w id',
-    id 
-  })
-})
-
+  Product.remove({ _id: id })
+    .exec()
+    .then(result => {
+      res.status(200).json(result);
+    })
+    .catch(error => {
+      res.status(500).json({error});
+    });
+});
 
 module.exports = router;
