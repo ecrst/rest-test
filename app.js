bed5978446a4d976e2694de07a69ae99b9920d9a
Eugene Crystal <ugncry@gmail.com>
2018-11-06 09:54:41 +0300

    feat: mongoose, dotenv

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
+
+mongoose.connect(
+  `mongodb+srv://${process.env.DB_USER}:${encodeURIComponent(process.env.DB_PASSWORD)}@rest-test-hqs9m.mongodb.net/test?retryWrites=true`,
+  { useNewUrlParser: true }
+)
 
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({ extended: false }));
